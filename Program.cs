5934ded58e400af5f9881ec4852b06cf15a76232
Arsenicu <kubarodak2003@outlook.com>
2024-07-23 19:21:02 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OVR_App_Template
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
